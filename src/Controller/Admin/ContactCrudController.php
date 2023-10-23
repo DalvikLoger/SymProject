f5815cf0323c6fc96dfae36f17d5868aa5565d5e
Dalvik <99361351+DalvikLoger@users.noreply.github.com>
2023-10-23 16:04:32 +0200

    Adding Dashboard

---
+++ src/Controller/Admin/ContactCrudController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Contact;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class ContactCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Contact::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural('Demande de contact')
+            ->setEntityLabelInSingular('Demandes de contact')
+
+            ->setPageTitle("index", "SymRecipe - Administration des demandes de contact")
+
+            ->setPaginatorPageSize(10)
+            
+            ->addFormTheme('@FOSCKEditor/Form/ckeditor_widget.html.twig');
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')
+                ->hideOnForm(),
+            TextField::new('fullName'),
+            TextField::new('email')
+                ->hideOnForm(),
+            TextareaField::new('message')
+                ->setFormType(CKEditorType::class)
+                ->hideOnIndex(),
+            DateTimeField::new('createdAt')
+                ->hideOnForm()
+        ];
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,6 +7,7 @@ use App\Entity\Mark;
 use App\Entity\User;
 use Faker\Generator;
 use App\Entity\Recipe;
+use App\Entity\Contact;
 use App\Entity\Ingredient;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -24,6 +25,17 @@ class AppFixtures extends Fixture
     {
         //Users
         $users = [];
+        $admin = new User();
+
+        $admin->setFullName('Administrateur de SymRecipe')
+            ->setPseudo(null)
+            ->SetEmail('admin@symrecipe.fr')
+            ->setRoles(['ROLE_USER', 'ROLE_ADMIN'])
+            ->setPassword('password');
+
+        $users[] = $admin;
+        $manager->persist($admin);
+        
     for ($i = 0; $i < 10; $i++) {
         $user = new User();
         $user->setFullName($this->faker->name())
@@ -80,6 +92,18 @@ class AppFixtures extends Fixture
                 $manager->persist($mark);
             }
         }
+
+        // Contact
+        for ($i = 0; $i < 5; $i++) {
+            $contact = new Contact();
+            $contact->setFullName($this->faker->name())
+                ->setEmail($this->faker->email())
+                ->setSubject('Demande n°' . ($i + 1))
+                ->setMessage($this->faker->text());
+
+            $manager->persist($contact);
+        }
+
 
 
         $manager->flush();
